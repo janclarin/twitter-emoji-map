1acd07e13213bee4a379da1dfb96e620e0c10703
Jan Clarin <hi@janclarin.com>
2016-09-17 14:25:35 -0400

    set index route to return index.html

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
 
 module.exports = router;
